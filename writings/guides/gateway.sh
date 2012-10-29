b8c2991ef97dc97766dc9ce9c4482955d9206313
Troels Henriksen <athas@sigkill.dk>
2012-10-29 20:39:25 +0100

    Fix gateway script

--- writings/guides/gateway.sh
+++ writings/guides/gateway.sh
@@ -5,13 +5,13 @@
 internal=eth0  # From whence we receive packets from the client.
 external=wlan0 # From whence we have access to the Internet.
 
-# Delete all rules in the NAT chain.
-iptables --table NAT --flush
-iptables --table NAT --delete-chain
+# Delete all rules in the 'nat' chain.
+iptables --table nat --flush
+iptables --table nat --delete-chain
 
 # Set up IP forwarding and masquerading (this is the actual NAT
 # stuff).
-iptables --table NAT --append POSTROUTING --out-interface $external -j MASQUERADE
+iptables --table nat --append POSTROUTING --out-interface $external -j MASQUERADE
 iptables --append FORWARD --in-interface $internal -j ACCEPT
 
 # Enable packet forwarding by kernel.
